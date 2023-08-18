019251c57a7feb87927bc4df70156779a07c34fe
ari <ari.hayrinen@gmail.com>
2023-08-18 17:00:06 +0300

    implemented pdf2images and pdfimages

--- README.md
+++ README.md
@@ -33,32 +33,39 @@ This will return JSON with "store" URL, where one can download the result.
 Register with service.json (this might change)
 
 	{
-	    "url": "http://localhost:8300",
-	    "id": "md-poppler",
-	    "type": "pdf",
-	    "api_type": "elg",
-	    "api": "/process",
-	    "name" :"Poppler",
-	    
-	    "supported_types": ["pdf"],
-	    "supported_formats": ["pdf"],
+		"url": "http://localhost:8300",
+		"id": "md-poppler",
+		"type": "pdf",
+		"api_type": "elg",
+		"api": "/process",
+		"name" :"Poppler",
+		
+		"supported_types": ["pdf"],
+		"supported_formats": ["pdf"],
 
-	    "description": "Poppler is a PDF rendering library that also includes a collection of utility binaries, which allows for the manipulation and extraction of data from PDF documents such as converting PDF files to HTML, TXT, or PostScript",
+		"description": "Poppler is a PDF rendering library that also includes a collection of utility binaries, which allows for the manipulation and extraction of data from PDF documents such as converting PDF files to HTML, TXT, or PostScript",
 
-	    "crunchers": {
-		"pdf2text": {
-		    "params": {"task": "pdf2text"},
-		    "output": "store",
-		    "name": "Extract text from PDF",
-		    "description": "Creates a txt file from PDF."
-		},
-		"pdf2images": {
-		    "params": {"task": "pdf2images"},
-		    "output": "store",
-		    "name": "Extract images from PDF",
-		    "description": "Creates separate image files from images in PDF file."
+		"tasks": {
+			"pdf2text": {
+				"params": {"task": "pdf2text"},
+				"output": "store",
+				"name": "Extract text from PDF",
+				"description": "Creates a txt file from PDF."
+			},
+			"pdf2images": {
+				"params": {"task": "pdf2images"},
+				"output": "store",
+				"name": "Render PDF as images",
+				"description": "Renders pages as image files in any resolution."           
+			},
+			"pdfimages": {
+				"params": {"task": "pdfimages"},
+				"output": "store",
+				"name": "Extract images from PDF (png)",
+				"description": "Extract images from PDF as png files."           
+			}
 		}
-	    }
+
 	}
 
 
--- index.js
+++ index.js
@@ -57,6 +57,8 @@ router.post('/process', upload.fields([
             output.response.uri = await PDFToText(contentFilepath, requestJSON.params.options, dirname)
         } else if(requestJSON.params.task == 'pdf2images') {
             output.response.uri = await PDFToImages(contentFilepath, requestJSON.params.options, dirname)
+        } else if(requestJSON.params.task == 'pdfimages') {
+            output.response.uri = await ImagesFromPDF(contentFilepath, requestJSON.params.options, dirname)
         }
 
         await fs.unlink(contentFilepath)
@@ -104,12 +106,13 @@ async function PDFToText(filepath, options, dirname) {
     if(!options) {
         options = {}
     }
-    var filename = filepath.split('/').pop()
+
     const poppler = new Poppler('/usr/bin/');
     await poppler.pdfToText(filepath, `data/${dirname}/text.txt`, options);
 
     return `/files/${dirname}/text.txt`
 }
+
 
 async function PDFToImages(filepath, options, dirname) {
     if(!options) {
@@ -119,33 +122,27 @@ async function PDFToImages(filepath, options, dirname) {
     if(!options.cropBox) options.cropBox = true
     //options.resolutionXYAxis = parseInt(params.resolution)
 
-    var filename = filepath.split('/').pop()
     const poppler = new Poppler('/usr/bin/');
-
-    console.log(filename)
     await poppler.pdfToPpm(filepath, `data/${dirname}/page`, options);
-    var images = getImageList(`data/${dirname}`,`files/${dirname}`)
+    var images = getImageList(`data/${dirname}`,`/files/${dirname}`)
+    console.log(images)
     return images
 
-
-//     if(!params.resolution || !parseInt(params.resolution)) throw('Invalid render resolution (must be integer)')
-//     const {filename, input_path, output_path} = await this.getPaths(params, 'rendered/' + params.resolution)
-//     if(await this.exists(output_path)) throw(`Output directory exists (${output_path})`)
-
-//     options.resolutionXYAxis = parseInt(params.resolution)
-//     if(query.format && ['jpg', 'jpeg'].includes(query.format)) options.jpegFile = true
+ }
 
 
-//     //const filepath = path.join(input_path, filename)
-//     if(!await this.exists(filepath)) throw('PDF file not found: ' + filepath)
-//     await fsp.mkdir(output_path, { recursive: true })
-//     console.log(output_path)
+ async function ImagesFromPDF(filepath, options, dirname) {
+    if(!options) {
+        options = {}
+    }
+    options.pngFile = true
+    options.allFiles = true
 
-//     const poppler = new Poppler('/usr/bin/');
-//     await poppler.pdfToPpm(filepath, output_path + '/page', options);
-//     var files = await this.getImageList(output_path, '')
-//     var response = {files: files}
-//     return response
+    const poppler = new Poppler('/usr/bin/');
+    await poppler.pdfImages(filepath, `data/${dirname}/page`, options)
+    var images = await getImageList(`data/${dirname}`,`/files/${dirname}`)
+    console.log(images)
+    return images
  }
 
 
